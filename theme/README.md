e9d5c1827ea88d417528d2ece9ed19feb4c49059
eugentaga <eugentaga@gmail.com>
2020-09-07 15:10:04 +0300

    1

---
+++ theme/README.md
@@ -0,0 +1,2 @@
+# _sbs4
+_S theme with Bootstrap 4.5
---
+++ theme/sidebar.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WpStarter
+ */
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
+?>
+
+<div class="col-12 col-md-4">
+      <aside id="secondary" class="widget-area">
+        <?php dynamic_sidebar( 'sidebar-1' ); ?>
+      </aside><!-- #secondary -->
+    </div><!-- .col-12 .col-md-4-->
+  </div><!-- .row -->
+</div><!-- .container -->
